13c4bb5bf87c5d335262f93ec92f4ac869cea63f
kadenh <kadenlukeharris6@gmail.com>
2026-03-10 20:11:38 -0700

    connect application routes to backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import applicationRoutes from './routes/applicationRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use("/applications", applicationRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
