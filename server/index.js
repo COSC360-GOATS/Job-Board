39c90f06a895d108464b56b634a4fd60cc477c29
kadenh <kadenlukeharris6@gmail.com>
2026-03-19 09:06:53 -0700

    create employer data, service, controller, and routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import applications from './routes/applicationRoutes.js';
 import applicants from './routes/applicantRoutes.js';
 import jobs from './routes/jobRoutes.js';
+import employers from './routes/employerRoutes.js';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use("/applications", applications);
 app.use("/applicants", applicants);
 app.use("/jobs", jobs);
+app.use("/employers", employers);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
