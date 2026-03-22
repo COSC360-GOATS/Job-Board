30a82f19e2d0312c939accad5445cc4bc0aa5d72
kadenh <kadenlukeharris6@gmail.com>
2026-03-22 09:24:29 -0700

    added job details page (using react-router-dom)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import NavBar from './components/NavBar'
 import './App.css'
 import JobDashboard from './components/employers/JobDashboard'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import JobOverview from './components/employers/JobDetails'
 
 function App() {
   return (
-    <main>
-      <NavBar />
-      <JobDashboard />
-    </main>
+    <Router>
+      <main>
+        <NavBar />
+        <Routes>
+          <Route path="/jobs" element={<JobDashboard />} />
+          <Route path="/jobs/:id" element={<JobOverview />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
--- src/components/employers/JobCard.jsx
+++ src/components/employers/JobCard.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom";
+
 const defaultJob = {
+    id: 1,
     title: 'Software Engineer',
     description: 'We are looking for a skilled software engineer to join our team. The ideal candidate will have experience with JavaScript, React, and Node.js. You will be responsible for developing and maintaining our web applications. This is a great opportunity to work on exciting projects and grow your career.',
     payRange: {
@@ -8,8 +11,10 @@ const defaultJob = {
 };
 
 function JobCard({ job = defaultJob }) {
+    const navigate = useNavigate();
+
     return (
-        <div className="text-white p-8 rounded-lg border border-gray-300 max-w-lg flex flex-col gap-3">
+        <div onClick={() => navigate(`/jobs/${job.id}`, { state: { job } })} className="text-white p-8 rounded-lg border border-gray-300 max-w-lg flex flex-col gap-3 cursor-pointer hover:bg-gray-800">
             <h3 className="text-3xl font-semibold">{job.title}</h3>
             <h6 className="text-lg">${job.payRange.low} - ${job.payRange.high}</h6>
             <p className="text-gray-400 line-clamp-3">{job.description}</p>
