67b4133a1f01a5292882bdad7618c7141cfe81dd
kadenh <kadenlukeharris6@gmail.com>
2026-03-29 16:21:13 -0700

    update navbar to use react-router-dom and implement placeholder pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import NavBar from './components/NavBar'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <main>
       <NavBar />
+      <Routes>
+        {/* TEMPORARY Routes, once pages are implemented replace these with the correct component */}
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/jobs" element={<h1>Jobs</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        <Route path="/create" element={<h1>Create</h1>} />
+        <Route path="/listings" element={<h1>Listings</h1>} />
+        <Route path="/signout" element={<h1>Sign Out</h1>} />
+      </Routes>
     </main>
   )
 }
