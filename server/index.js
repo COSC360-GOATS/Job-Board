1251ac909011af1c6db42571edfac40dd1ca6332
harkirat-c <harkirat5252703@gmail.com>
2026-04-02 01:28:59 -0700

    Updated index.js to include new auth/login routes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+
 import applications from './routes/applicationRoutes.js';
 import applicants from './routes/applicantRoutes.js';
 import jobs from './routes/jobRoutes.js';
 import employers from './routes/employerRoutes.js';
 import ratings from './routes/ratingRoutes.js';
+import auth from './routes/authRoutes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/applications", applications);
 app.use("/applicants", applicants);
 app.use("/jobs", jobs);
 app.use("/employers", employers);
 app.use("/ratings", ratings);
+app.use("/auth", auth);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
