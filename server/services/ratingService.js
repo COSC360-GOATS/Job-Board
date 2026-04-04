ae30664afa548f7a49047a047eefcd695571b637
kadenh <kadenlukeharris6@gmail.com>
2026-04-04 08:04:02 -0700

    create new routes for getting ratings for specific employers and getting their average ratings

--- server/services/ratingService.js
+++ server/services/ratingService.js
@@ -1,5 +1,22 @@
+import { ObjectId } from "mongodb";
 import createService from "./service.js";
 
 export default function ratingService(db) {
-    return createService(db.collection('ratings'));
+    const collection = db.collection('ratings');
+    const service = createService(collection);
+    return {
+        ...service,
+
+        async getByEmployerId(employerId) {
+            if (!ObjectId.isValid(employerId)) return [];
+            return await collection.find({ employerId: employerId }).toArray();
+        },
+
+        async getAvgRatingForEmployer(employerId) {
+            const ratings = await this.getByEmployerId(employerId);
+            if (ratings.length === 0) return null;
+            const total = ratings.reduce((sum, r) => sum + r.rating, 0);
+            return total / ratings.length;
+        }
+    }
 }
