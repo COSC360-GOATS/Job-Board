06b240d876eae442542bdfd84bb843efa1dd42ef
harkirat-c <harkirat5252703@gmail.com>
2026-04-05 22:29:39 -0700

    wired applicant job routes and fixed sign out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import NavBar from './components/NavBar'
 import RegisterForm from './components/RegisterForm'
 import Login from './components/Login'
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import JobDashboard from './components/employers/JobDashboard'
 import JobOverview from './components/employers/JobDetails'
 import ApplicationList from './components/employers/ApplicationList'
 import AdminDashboard from './components/admin/AdminDashboard'
+import JobListings from './components/applicants/JobListings'
+import ApplyPage from './components/applicants/ApplyPage'
+import { useEffect } from 'react'
+
+function SignOut() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    localStorage.removeItem('user')
+    navigate('/')
+  }, [])
+  return null
+}
 
 function App() {
   const location = useLocation()
@@ -19,11 +31,12 @@ function App() {
         <Route path="/home" element={<h1>Home</h1>} />
         <Route path="/profile" element={<h1>Profile</h1>} />
         <Route path="/create" element={<h1>Create</h1>} />
-        <Route path="/listings" element={<h1>Listings</h1>} />
-        <Route path="/signout" element={<h1>Sign Out</h1>} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signout" element={<SignOut />} />
+        <Route path="/listings" element={<JobListings />} />
+        <Route path="/listings/:jobId" element={<ApplyPage />} />
         <Route path="/jobs" element={<JobDashboard />} />
         <Route path="/jobs/:jobId" element={<JobOverview />} />
         <Route path="/jobs/:jobId/applications" element={<ApplicationList />} />
